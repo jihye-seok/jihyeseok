993962ef070b42c8e950b1392ca88d6d691375c2
jihyeseok <hye_xx@naver.com>
2020-08-13 19:10:12 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react=dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceworker from './serviceworker';
+import * as serviceWorker from './serviceWorker.js';
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
@@ -15,4 +15,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes sith some pitfalls
 // rearn more about service workers: http://bit.ly/CRA-PWA
-serviceworker.unregister();
+serviceWorker.unregister();
