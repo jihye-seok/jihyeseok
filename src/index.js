435657587330ae2378cd7eb9580970bdb78fbc0b
jihyeseok <hye_xx@naver.com>
2020-08-13 19:17:59 +0900

    1일차 수정
    1

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
-  <React.strictMode>
-  <App />
-  </React.strictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes sith some pitfalls
-// rearn more about service workers: http://bit.ly/CRA-PWA
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
