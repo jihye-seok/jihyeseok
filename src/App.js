47cbad7c3d24519550b9bb66db8c810aa016f4f4
jihyeseok <hye_xx@naver.com>
2020-08-16 18:17:50 +0900

    3일차
    3

--- src/App.js
+++ src/App.js
@@ -1,59 +1,70 @@
-import React from 'react';
-import {Button, Image, Divider, Header, Icon, Grid} from 'semantic-ui-react'
+import React from "react";
+import { Button, Image, Divider, Header, Icon, Grid } from "semantic-ui-react";
 
-import Comments from './comments.js'
+import Comments from "./comments.js";
 
-import ham from"./ham.jpg"
+import ham from "./ham.jpg";
 
 function App() {
   return (
     <div>
-    <Grid centered>
-    <Grid.Row>
-      <Image src = {ham} centered />
-</Grid.Row>
-<Grid.Row>
-    <Button
-      color='red'
-      content='Like'
-      icon='heart'
-      label={{ basic: true, color: 'red', pointing: 'left', content: '0' }}
-    />
-    <Button
-      basic
-      color='blue'
-      content='Share'
-      icon='fork'
-      label={{
-        as: 'a',
-        basic: true,
-        color: 'blue',
-        pointing: 'left',
-        content: '0',
-      }}
-    />
-    </Grid.Row>
-  </Grid>
-  <br/>
-    <Divider horizontal>
-  <Header as='h4'>
-    <Icon name='envelope outline' />
-    Contact me
-  </Header>
-</Divider>
-<br/>
-    <Comments />
+      <Grid centered>
+        <Grid.Row>
+          <Image src={ham} centered />
+        </Grid.Row>
+        <Grid.Row>
+          <Button
+            color="red"
+            content="Like"
+            icon="heart"
+            label={{
+              basic: true,
+              color: "red",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+          <Button
+            basic
+            color="blue"
+            content="Share"
+            icon="fork"
+            label={{
+              as: "a",
+              basic: true,
+              color: "blue",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+        </Grid.Row>
+      </Grid>
 
-    <div>
-    <Grid centered>
-<Button circular color='facebook' icon='facebook' />
-<Button circular color='twitter' icon='twitter' />
-<Button circular color='youtuve' icon='youtuve' />
-<Button circular color='google plus' icon='google plus' />
-</Grid>
-</div>
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="pencil alternate" />
+          댓글달아줭
+        </Header>
+      </Divider>
 
+      <br />
 
+      <br />
+      <Comments />
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="envelope outline" />
+          Contact me
+        </Header>
+      </Divider>
+      <div>
+        <Grid centered>
+          <Button circular color="facebook" icon="facebook" />
+          <Button circular color="twitter" icon="twitter" />
+          <Button circular color="youtuve" icon="youtuve" />
+          <Button circular color="google plus" icon="google plus" />
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/comments.js
+++ src/comments.js
@@ -1,44 +1,48 @@
-import React from 'react';
-import {Comment, Form, Button, Header, Icon} from 'semantic-ui-react'
+import React from "react";
+import { Comment, Form, Button, Header, Icon } from "semantic-ui-react";
 
 
-import human from"./human.png"
+import human from "./human.png";
 
 class Comments extends React.Component {
-
-  render(){
-    return(<Comment.Group style = {{marginLeft :"600px" }}>
-      <Header as='h3' dividing>
-        Comments
-      </Header>
-
-      <Comment>
-        <Comment.Avatar src={human} />
-        <Comment.Content>
-          <Comment.Author as='a'>Matt</Comment.Author>
-          <Comment.Metadata>
-            <div>Today at 5:42PM</div>
-          </Comment.Metadata>
-          <Comment.Text>How artistic!</Comment.Text>
-        </Comment.Content>
-      </Comment>
-
-
-
-      <Form reply>
-        <Form.TextArea />
-        <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-      </Form>
-      <br/>
-
-        <div>
-   <Button circular color='facebook' icon='facebook' />
-   <Button circular color='twitter' icon='twitter' />
-   <Button circular color='youtuve' icon='youtuve' />
-   <Button circular color='google plus' icon='google plus' />
- </div>
-
-    </Comment.Group>)
+constructor(){
+  super()
+  this.state = {
+    inputContent : "댓글을 입력해주세요!"
   }
 }
-export default Comments
+
+
+  render() {
+    return (
+      <Comment.Group style={{ marginLeft: "600px" }}>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
+
+        <Comment>
+          <Comment.Avatar src={human} />
+          <Comment.Content>
+            <Comment.Author as="a">Matt</Comment.Author>
+            <Comment.Metadata>
+              <div>Today at 5:42PM</div>
+            </Comment.Metadata>
+            <Comment.Text>How artistic!</Comment.Text>
+          </Comment.Content>
+        </Comment>
+
+        <Form reply>
+          <Form.TextArea value = {this.state.inputContent}>
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary />
+          </Form.TextArea>
+        </Form>
+        <br />
+      </Comment.Group>
+    );
+  }
+}
+export default Comments;
