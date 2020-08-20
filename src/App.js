4cf0a7ae71e25b2ed3e7c71e5c1179cbcbb7f81e
jihyeseok <hye_xx@naver.com>
2020-08-20 18:22:41 +0900

    4일차(3)
    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from "react";
-import { Button, Image, Divider, Header, Icon, Grid } from "semantic-ui-react";
+import firebase from "firebase"
+import {
+  Button,
+  Image,
+  Divider,
+  Header,
+  Icon,
+  Grid,
+  Input,
+  Menu,
+} from "semantic-ui-react";
 
 import Comments from "./comments.js";
-import Buttons from "./buttons.js"
+import Buttons from "./buttons.js";
+import auth from "./fb.js";
+
 import ham from "./ham.jpg";
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 function App() {
   return (
-        <div style = {{backgroundColor : "black"}}>
+    <div style={{ backgroundColor: "black" }}>
+      <h1 style={{ color: "white", textAlign: "center", paddingTop: "15px" }}>
+        {" "}
+        Kimbutter{" "}
+      </h1>
+      <Menu inverted widths={3}>
+        <Menu.Item> Menu </Menu.Item>
+        <Menu.Item> 반가워요, 방문자 </Menu.Item>
+        <Menu.Item
+          onClick={() => {
+            firebase
+              .auth()
+              .signInWithPopup(provider)
+              .then(function (result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+              })
+              .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+              });
+          }}
+        >
+          {" "}
+          Login{" "}
+        </Menu.Item>
+      </Menu>
+      <Divider horizontal>
+        <Header as="h4" style={{ color: "white" }}>
+          <Icon name="paint brush" style={{ color: "white" }} />
+          My Artwork
+        </Header>
+      </Divider>
       <Grid centered>
         <Grid.Row>
           <Image src={ham} centered />
         </Grid.Row>
         <Grid.Row>
-<Buttons/>
+          <Buttons />
         </Grid.Row>
       </Grid>
 
       <Divider horizontal>
-        <Header as="h4" style = {{color : "white"}}>
-          <Icon name="pencil alternate" style = {{color : "white"}} />
-      댓글을 입력해주세요
+        <Header as="h4" style={{ color: "white" }}>
+          <Icon name="pencil alternate" style={{ color: "white" }} />
+          댓글을 입력해주세요
         </Header>
       </Divider>
 
@@ -29,8 +84,8 @@ function App() {
       <br />
       <Comments />
       <Divider horizontal>
-        <Header as="h4" style = {{color : "white"}}>
-          <Icon name="envelope outline" style = {{color : "white"}} />
+        <Header as="h4" style={{ color: "white" }}>
+          <Icon name="envelope outline" style={{ color: "white" }} />
           Contact me
         </Header>
       </Divider>
@@ -38,8 +93,16 @@ function App() {
         <Grid centered>
           <Button circular color="instagram" icon="instagram" />
           <Button circular color="twitter" icon="twitter" />
-          <Button circular color="youtube" icon="youtube" onClick = {()=>window.open(
-            "https://www.youtube.com/channel/UC7ue1pZBUYiz_S3OKE7g_cQ/featured")}  />
+          <Button
+            circular
+            color="youtube"
+            icon="youtube"
+            onClick={() =>
+              window.open(
+                "https://www.youtube.com/channel/UC7ue1pZBUYiz_S3OKE7g_cQ/featured"
+              )
+            }
+          />
           <Button circular color="google plus" icon="google plus" />
         </Grid>
       </div>
